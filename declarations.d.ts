5171bed396726156e03abba9d7dd1c8586215d43
Marius ballot <marius@haply.co>
2023-09-24 14:35:06 +0200

    .glsl support

--- declarations.d.ts
+++ declarations.d.ts
@@ -7,3 +7,8 @@ declare module '*.frag' {
     const value: string;
     export default value;
 }
+
+declare module '*.glsl' {
+    const value: string;
+    export default value;
+}
--- src/components/three-scene/webgl-scripts/ExampleObject.ts
+++ src/components/three-scene/webgl-scripts/ExampleObject.ts
@@ -1,46 +1,45 @@
 import * as THREE from "three";
-import fragShader from "./shaders/dummy.frag"
-import vertShader from "./shaders/dummy.vert"
+import dummyFrag from "./shaders/dummyFrag.glsl"
+import dummyVert from "./shaders/dummyVert.glsl"
 
 class ExampleObject {
-    private scene: THREE.Scene | undefined;
-    private texLoader: THREE.TextureLoader;
-    private dummy: THREE.Mesh;
+  private scene: THREE.Scene | undefined;
+  private texLoader: THREE.TextureLoader;
+  private dummy: THREE.Mesh;
 
-    constructor() {
-        this.bind()
-        this.texLoader = new THREE.TextureLoader()
-        this.dummy = new THREE.Mesh(new THREE.IcosahedronGeometry(), new THREE.MeshNormalMaterial())
-    }
+  constructor() {
+    this.bind()
+    this.texLoader = new THREE.TextureLoader()
+    this.dummy = new THREE.Mesh(new THREE.IcosahedronGeometry(), new THREE.MeshNormalMaterial())
+  }
 
-    init(scene: THREE.Scene) {
-    // init(scene) {
-        this.scene = scene
-        this.scene.add(this.dummy)
+  init(scene: THREE.Scene) {
+    this.scene = scene
+    this.scene.add(this.dummy)
 
-        const matcapTex1 = this.texLoader.load("/assets/textures/black-metal-matcap.png")
-        matcapTex1.colorSpace = THREE.SRGBColorSpace
+    const matcapTex1 = this.texLoader.load("/assets/textures/black-metal-matcap.png")
+    matcapTex1.colorSpace = THREE.SRGBColorSpace
 
-        this.dummy.material = new THREE.ShaderMaterial({
-            uniforms:{
-                u_metalMatCap:{
-                    value:matcapTex1
-                }
-            },
-            vertexShader: vertShader,
-            fragmentShader: fragShader,
-        })
+    this.dummy.material = new THREE.ShaderMaterial({
+      uniforms: {
+        u_metalMatCap: {
+          value: matcapTex1
+        }
+      },
+      vertexShader: dummyVert,
+      fragmentShader: dummyFrag,
+    })
 
 
-    }
+  }
 
-    update() {
-        this.dummy.rotateX(0.01)
-        this.dummy.rotateY(0.005)
-    }
+  update() {
+    this.dummy.rotateX(0.01)
+    this.dummy.rotateY(0.005)
+  }
 
-    bind() {
-    }
+  bind() {
+  }
 
 }
 
