1cd7b32a50a24485b56d7808ec3c607826c2b28a
Marius ballot <marius@haply.co>
2023-10-02 14:54:57 +0200

    delta time

--- src/components/three-scene/webgl-scripts/MainThreeScene.ts
+++ src/components/three-scene/webgl-scripts/MainThreeScene.ts
@@ -4,70 +4,69 @@ import RAF from '../../../utils/RAF';
 import ExampleObject from "./ExampleObject"
 
 class MainThreeScene {
-    private camera: THREE.PerspectiveCamera;
-    private scene: THREE.Scene;
-    private renderer: THREE.WebGLRenderer;
-    private controls: OrbitControls;
-    private isActive: Boolean
+  private camera: THREE.PerspectiveCamera;
+  private scene: THREE.Scene;
+  private renderer: THREE.WebGLRenderer;
+  private controls: OrbitControls;
+  private isActive: Boolean
 
-    constructor() {
-        this.bind();
-        this.isActive = false;
+  constructor() {
+    this.bind();
+    this.isActive = false;
 
-        // RENDERER SETUP
-        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.debug.checkShaderErrors = true;
+    // RENDERER SETUP
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.debug.checkShaderErrors = true;
 
-        this.scene = new THREE.Scene();
-
-
-        // CAMERA AND ORBIT CONTROLLER
-        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.set(0, 0, 10);
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-        this.controls.enabled = true; // You can adjust this based on your config.
-        this.controls.maxDistance = 1500;
-        this.controls.minDistance = 0;
+    this.scene = new THREE.Scene();
 
 
-    }
-
-    init(container: HTMLElement) {
-        if (this.isActive) return
-        this.isActive = true
-
-        container.appendChild(this.renderer.domElement);
-
-        // MAIN SCENE INSTANCE
+    // CAMERA AND ORBIT CONTROLLER
+    this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.set(0, 0, 10);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enabled = true; // You can adjust this based on your config.
+    this.controls.maxDistance = 1500;
+    this.controls.minDistance = 0;
 
 
+  }
 
-        ExampleObject.init(this.scene)
+  init(container: HTMLElement) {
+    if (this.isActive) return
+    this.isActive = true
 
-        // RENDER LOOP AND WINDOW SIZE UPDATER SETUP
-        window.addEventListener("resize", () => this.resizeCanvas());
-        RAF.subscribe('threeSceneUpdate', () => this.update());
-    }
+    container.appendChild(this.renderer.domElement);
 
-    update() {
-        ExampleObject.update()
+    // MAIN SCENE INSTANCE
 
-        this.renderer.render(this.scene, this.camera);
-    }
 
-    resizeCanvas() {
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
 
-    }
+    ExampleObject.init(this.scene)
 
-    private bind() {
-        this.resizeCanvas = this.resizeCanvas.bind(this);
-        this.update = this.update.bind(this);
-        this.init = this.init.bind(this);
-    }
+    // RENDER LOOP AND WINDOW SIZE UPDATER SETUP
+    window.addEventListener("resize", () => this.resizeCanvas());
+    RAF.subscribe('threeSceneUpdate', () => this.update());
+  }
+
+  update() {
+    ExampleObject.update()
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  resizeCanvas() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+
+  }
+
+  private bind() {
+    this.resizeCanvas = this.resizeCanvas.bind(this);
+    this.update = this.update.bind(this);
+    this.init = this.init.bind(this);
+  }
 }
 
 const _instance = new MainThreeScene();
--- src/utils/RAF.ts
+++ src/utils/RAF.ts
@@ -1,41 +1,46 @@
 class RAF {
-    private callbacks: { name: string; callback: () => void }[];
+  private callbacks: { name: string; callback: () => void }[];
+  private lastTimestamp: number;
+  public dt: number;
 
-    constructor() {
-        this.bind();
-        this.callbacks = [];
-        this.render();
-    }
+  constructor() {
+    this.bind();
+    this.callbacks = [];
+    this.lastTimestamp = 0;
+    this.dt = 0;
+    this.render(0);
+  }
 
-    subscribe(name: string, callback: () => void) {
-        this.callbacks.push({
-            name: name,
-            callback: callback,
-        });
-    }
+  subscribe(name: string, callback: () => void) {
+    this.callbacks.push({
+      name: name,
+      callback: callback,
+    });
+  }
 
-    unsubscribe(name: string) {
-        this.callbacks.forEach((item, i) => {
-            if (item.name === name) {
-                this.callbacks.splice(i, 1);
-            }
-        });
-    }
+  unsubscribe(name: string) {
+    this.callbacks = this.callbacks.filter(item => item.name !== name);
+  }
 
-    render() {
-        requestAnimationFrame(this.render);
-        this.callbacks.forEach((item) => {
-            item.callback();
-        });
+  render(timestamp: number) {
+    if (!this.lastTimestamp) {
+      this.lastTimestamp = timestamp;
     }
+    this.dt = timestamp - this.lastTimestamp; // Convert to seconds
+    this.lastTimestamp = timestamp;
 
-    private bind() {
-        this.subscribe = this.subscribe.bind(this);
-        this.unsubscribe = this.unsubscribe.bind(this);
-        this.render = this.render.bind(this);
-    }
+    requestAnimationFrame(this.render.bind(this));
+    this.callbacks.forEach((item) => {
+      item.callback();
+    });
+  }
+
+  private bind() {
+    this.subscribe = this.subscribe.bind(this);
+    this.unsubscribe = this.unsubscribe.bind(this);
+    this.render = this.render.bind(this);
+  }
 }
 
-
-const _instance = new RAF()
-export default _instance
+const _instance = new RAF();
+export default _instance;
