23d0e76a9e08ab6002592f0a1280b9f1c9f8ea95
MariusBallot <ballot.marius@gmail.com>
2023-10-02 09:40:23 +0200

    constructor init remake

--- src/components/three-scene/webgl-scripts/MainThreeScene.ts
+++ src/components/three-scene/webgl-scripts/MainThreeScene.ts
@@ -1,44 +1,47 @@
 import * as THREE from "three";
-import { OrbitControls } from 'three-orbitcontrols-ts';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import RAF from '../../../utils/RAF';
 import ExampleObject from "./ExampleObject"
 
 class MainThreeScene {
-    private camera: THREE.PerspectiveCamera | undefined;
-    private scene: THREE.Scene | undefined;
-    private renderer: THREE.WebGLRenderer | undefined;
-    private controls: OrbitControls | undefined;
+    private camera: THREE.PerspectiveCamera;
+    private scene: THREE.Scene;
+    private renderer: THREE.WebGLRenderer;
+    private controls: OrbitControls;
     private isActive: Boolean
 
     constructor() {
         this.bind();
-        this.isActive = false
-    }
+        this.isActive = false;
 
-    init(container: HTMLElement) {
-        if(this.isActive) return
-        this.isActive = true
-        console.log(container)
         // RENDERER SETUP
         this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.renderer.debug.checkShaderErrors = true;
-        container.appendChild(this.renderer.domElement);
 
-        // MAIN SCENE INSTANCE
         this.scene = new THREE.Scene();
+
 
         // CAMERA AND ORBIT CONTROLLER
         this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-        if (this.camera) {
-            this.camera.position.set(0, 0, 10);
-            this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-            if (this.controls) {
-                this.controls.enabled = true; // You can adjust this based on your config.
-                this.controls.maxDistance = 1500;
-                this.controls.minDistance = 0;
-            }
-        }
+        this.camera.position.set(0, 0, 10);
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enabled = true; // You can adjust this based on your config.
+        this.controls.maxDistance = 1500;
+        this.controls.minDistance = 0;
+
+
+    }
+
+    init(container: HTMLElement) {
+        if (this.isActive) return
+        this.isActive = true
+
+        container.appendChild(this.renderer.domElement);
+
+        // MAIN SCENE INSTANCE
+
+
 
         ExampleObject.init(this.scene)
 
@@ -50,17 +53,14 @@ class MainThreeScene {
     update() {
         ExampleObject.update()
 
-        if (this.renderer && this.scene && this.camera) {
-            this.renderer.render(this.scene, this.camera);
-        }
+        this.renderer.render(this.scene, this.camera);
     }
 
     resizeCanvas() {
-        if (this.renderer && this.camera) {
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-            this.camera.aspect = window.innerWidth / window.innerHeight;
-            this.camera.updateProjectionMatrix();
-        }
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
     }
 
     private bind() {
